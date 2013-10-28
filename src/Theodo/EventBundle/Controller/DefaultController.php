96249e5932f5eb464533038c94f73965961a2b71
Vincent Perez <vincentp@theodo.fr>
2013-10-28 19:13:24 +0100

    ajout fichiers event bundle

---
+++ src/Theodo/EventBundle/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Theodo\EventBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name, $count)
+    {
+        /*$templating = $this->container->get('templating');
+
+
+        $content = $templating->render(
+            'EventBundle:Default:index.html.twig',
+            array('name' => $name)
+        );
+
+        return new Response($content);*/
+
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('EventBundle:Event');
+        $event = $repo->findOneBy(array(
+            'name' => 'Darth\'s surprise birthday party',
+        ));
+
+
+
+        return $this->render('EventBundle:Default:index.html.twig',  array(
+            'name' => $name,
+            'count' => $count,
+            'event' =>$event
+        ));
+
+
+    }
+}
---
+++ src/Theodo/EventBundle/EventBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Theodo\EventBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EventBundle extends Bundle
+{
+}
