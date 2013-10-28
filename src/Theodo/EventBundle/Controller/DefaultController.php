0b66f329fad40b5cede7c232b7ec5b10cf096cb1
Vincent Perez <vincentp@theodo.fr>
2013-10-28 19:18:21 +0100

    removing data base access

--- src/Theodo/EventBundle/Controller/DefaultController.php
+++ src/Theodo/EventBundle/Controller/DefaultController.php
@@ -21,17 +21,17 @@ class DefaultController extends Controller
 
 
         $em = $this->getDoctrine()->getManager();
-        $repo = $em->getRepository('EventBundle:Event');
+        /*$repo = $em->getRepository('EventBundle:Event');
         $event = $repo->findOneBy(array(
             'name' => 'Darth\'s surprise birthday party',
         ));
-
+    */
 
 
         return $this->render('EventBundle:Default:index.html.twig',  array(
             'name' => $name,
             'count' => $count,
-            'event' =>$event
+           // 'event' =>$event
         ));
 
 
